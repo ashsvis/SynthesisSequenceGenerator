2e1d4c4ca0e973c4bec45b1051acd74594e80dc0
Александр Шаповалов <ashsvis@gmail.com>
2025-08-28 09:23:56 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SynthesisSequenceGenerator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
