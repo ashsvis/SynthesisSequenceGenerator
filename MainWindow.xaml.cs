752b6636183437f981179749569b5b2a634f25d6
Александр Шаповалов <ashsvis@gmail.com>
2025-08-29 19:49:55 +0300

    Сохраняем исходную строку для использования при следующей загрузки

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,8 +14,16 @@ namespace SynthesisSequenceGenerator
             model = (ModelGenerator)DataContext;
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SourceSequencesBox.Text = Properties.Settings.Default.SourceSequenses;
+            model.GenerateSequences(ModelGenerator.GenerateSourceArray(SourceSequencesBox.Text));
+        }
+
         private void SourceSequencesButton_Click(object sender, RoutedEventArgs e)
         {
+            Properties.Settings.Default.SourceSequenses = SourceSequencesBox.Text;
+            Properties.Settings.Default.Save();
             model.GenerateSequences(ModelGenerator.GenerateSourceArray(SourceSequencesBox.Text));
         }
     }
